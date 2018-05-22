c36f1fcc52dfa63cdf89ceb63c5065410c4672f2
MALIQI <909040169@qq.com>
2018-05-22 10:32:10 +0800

    this is my first commit!

--- soft/Objects/gui_x.d
+++
@@ -1,8 +0,0 @@
-.\objects\gui_x.o: Emwin\OS\GUI_X.c
-.\objects\gui_x.o: .\Emwin\inc\GUI.h
-.\objects\gui_x.o: .\Emwin\inc\GUI_ConfDefaults.h
-.\objects\gui_x.o: .\Emwin\Config\GUIConf.h
-.\objects\gui_x.o: .\Emwin\inc\GUI_Type.h
-.\objects\gui_x.o: .\Emwin\inc\LCD.h
-.\objects\gui_x.o: .\Emwin\inc\Global.h
-.\objects\gui_x.o: .\Emwin\inc\GUI_Version.h
--- soft/Objects/queue.d
+++
@@ -1,5 +0,0 @@
-.\objects\queue.o: User\src\Queue.c
-.\objects\queue.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-.\objects\queue.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
-.\objects\queue.o: .\User\inc\Queue.h
-.\objects\queue.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
